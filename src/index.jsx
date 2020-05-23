98d64decfca869f235264f765c8abf4defa557fa
Will Macrae <wmacrae@palantir.com>
2020-05-22 21:26:20 -0700

    connect to backend

--- src/index.jsx
+++ src/index.jsx
@@ -2,23 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import io from 'socket.io-client';
 import reducer from './reducer';
 import App from './components/App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(reducer);
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    vote: {
-      pair: ['Frozen', 'Coco'],
-      tally: {
-        'Frozen': 2
-      }
-    }
-  }
-});
+
+const socket = io(`${window.location.protocol}//${window.location.hostname}:8090`);
+socket.on('state', state =>
+  store.dispatch({type: 'SET_STATE', state})
+);
 
 ReactDOM.render(<Provider store={store}>
     <App />
